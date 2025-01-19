66dd1a3860926c0c5111ba124735330932ffd1f7
T7Aslan <aslanvolk@bk.ru>
2025-01-19 18:18:59 +0300

    применение CARGO FMT

--- TASK8/src/main.rs
+++ TASK8/src/main.rs
@@ -3,10 +3,10 @@
 fn main() {
     let mass = [8, 1, 100];
     let [a, b, c] = mass;
-    let d = array_sum (a,b,c);
+    let d = array_sum(a, b, c);
     println!("Принимает кортеж из двух целых чисел {}, {}, {} . Возвращает целое число, равное сумме чисел во входном кортеже,{} ", a, b, c, d);
-    }  
-    
-    fn array_sum (a: i32, b:i32, c:i32) -> i32 {
-    a+b+c}
-    
+}
+
+fn array_sum(a: i32, b: i32, c: i32) -> i32 {
+    a + b + c
+}
